832023bbfc244d752bcd664ff7b6f3b9618c7586
SUMERA222 <83210529+Sumera222@users.noreply.github.com>
2021-08-31 10:33:40 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Main from './components/Main';
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    
+    <div className="App">
+      <h1 className='text-center' >  Contact-List </h1>
+      <Main></Main>
+      
+    </div>
+  );
+}
+
+export default App;
